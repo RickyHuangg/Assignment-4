16f6a51ab9e1e1b814033fc00d3829ad38f18b80
Ricky <Rhruan2@gmail.com>
2023-05-01 21:30:43 -0400

    start

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { TMDB_API_KEY } from "./key.js";
+import axois from "axios";
+import { ref } from "vue";
+
+const movieSelect = ref(null);
+const movieData = ref(false);
+
+const movieGet = async () => {
+  movieData.value = (
+    await axios.get(`https:api.themoviedb.org/3/movie/${movieSelect.value}}`, {
+      params: {
+        api_key: TMDB_API_KEY,
+        append_to_response: "videos",
+      },
+    })
+  ).data;
+};
+</script>
